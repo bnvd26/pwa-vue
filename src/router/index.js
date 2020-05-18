29bd834558930e2deb2a814391ec0a558e0155aa
benads <benjaminad5@yahoo.com>
2020-05-18 15:29:14 +0200

    feat(router) Dynamic Route Matching

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Post from "../views/Post.vue";
+import Article from "../views/Article.vue";
 
 Vue.use(VueRouter);
 
@@ -15,6 +16,11 @@ const routes = [
 		path: "/posts",
 		name: "Post",
 		component: Post,
+	},
+	{
+		path: "/post/:id",
+		name: "Article",
+		component: Article,
 	},
 ];
 
---
+++ src/views/Article.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="home">Article numéro {{ $route.params.id}}</div>
+</template>
+
+<script>
+export default {
+  name: "Article"
+};
+</script>
