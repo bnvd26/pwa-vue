c89a998f0f76274948c0802c3a93ef8a278fd092
benads <benjaminad5@yahoo.com>
2020-05-18 15:25:51 +0200

    feat(router) add post route

---
+++ src/views/Post.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Listing msg="Welcome to the post page" />
+  </div>
+</template>
+
+<script>
+
+import Listing from "@/components/Listing.vue";
+
+export default {
+  name: "Post",
+  components: {
+    Listing
+  }
+};
+</script>
