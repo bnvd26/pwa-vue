1952c957522b8ec75f5d1d555c1968d95bd35a96
benads <benjaminad5@yahoo.com>
2020-05-18 17:16:56 +0200

    add link in navbar of an example dynamic route matching

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/posts">Post</router-link>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/posts">Post</router-link> |
+      <router-link to="/post/12">Detail d'un article exemple</router-link>
     </div>
     <router-view />
   </div>
