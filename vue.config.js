245f8cf5dd4470b6d4e1213a4cdb5811c525b17e
Benjamin <benjaminad5@yahoo.com>
2020-05-19 12:06:38 +0200

    fix : favicon, logo of app changed

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,13 @@ module.exports = {
 		manifestOptions: {
 			display: "fullscreen",
 		},
+		iconPaths: {
+			favicon32: "images/icon/72x72.png",
+			favicon16: "images/icon/72x72.png",
+			appleTouchIcon: "images/icon/152x152.png",
+			maskIcon: "images/icon/72x72.png",
+			msTileImage: "images/icon/144x144.png",
+		},
 		workboxPluginMode: "InjectManifest",
 		workboxOptions: {
 			swSrc: "src/service-worker.js",
