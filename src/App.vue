0719b844da5f06499c92fea26b926c8a1a60efe7
Benjamin <benjaminad5@yahoo.com>
2020-05-19 18:04:24 +0200

    feat(network) add navigatorOnLine API

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <template>
   <div id="app">
+    <div v-if="!online">Vérifier votre connexion internet</div>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/posts">Post</router-link> |
+      <router-link to="/">Home</router-link>|
+      <router-link to="/posts">Post</router-link>|
       <router-link to="/post/12">Detail d'un article exemple</router-link>
     </div>
     <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      online: navigator.onLine
+    };
+  },
+  methods: {
+    updateOnlineStatus(e) {
+      const { type } = e;
+      this.online = type === "online";
+    }
+  },
+
+  mounted() {
+    window.addEventListener("online", this.updateOnlineStatus);
+    window.addEventListener("offline", this.updateOnlineStatus);
+  },
+  beforeDestroy() {
+    window.removeEventListener("online", this.updateOnlineStatus);
+    window.removeEventListener("offline", this.updateOnlineStatus);
+  }
+};
+</script>
 
 <style>
 #app {
