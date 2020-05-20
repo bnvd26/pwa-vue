865e140eb0f419dd907c3bad675811a9a7b3e12a
Benjamin <benjaminad5@yahoo.com>
2020-05-20 02:00:43 +0200

    refacto: remove unused code

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -1,79 +1,26 @@
 <template>
-
-  <div class="post">
-   <Listing msg=" article " /> 
-   <h2> TOUS NOS ARTICLES</h2>
-   
-
-   <div class="preview first">
-     <div class="object">
-        <h3> Reine de l'aventure </h3>
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, </p>
-        <p class="linkto"> lire l'article  <img alt="arrow" src="../assets/Arrow.svg" class="arrow"></p>
-        <span></span>
-      </div>
-      <div>
-       <img alt="arrow" src="../assets/slider-image.png" class="car">
-      </div>
-      
-   </div>
-
-    
-    <div class="preview second">
-     <div class="object">
-        <h3> Reine de l'aventure </h3>
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, </p>
-        <p> lire l'article  <img alt="arrow" src="../assets/Arrow.svg" class="arrow"></p>
-        <span></span>
-      </div>
-      <div>
-       <img alt="arrow" src="../assets/slider-image.png" class="car">
-      </div>
-    </div>
-  </div>
+	<div class="post">
+		<h2>TOUS NOS ARTICLES</h2>
+		<Listing />
+	</div>
 </template>
 
 <style>
-
-.post{
-  text-align: left;
-  width: 100%;
-  margin-left: 50%;
-  transform: translateX(-50%);
-}
-.preview{
-  margin-left: 100px;
-}
-.first, .second{
-  display : flex;
-  flex-direction: row;
-}
-.car{
-  margin-left: 50px;
-}
-.post h2{
-  margin-left: 50px;
-  margin-bottom: 50px;
-}
-.post span{
-  margin-top: 0;
-  margin-left: 0;
-}
-.second{
-  margin-top: 50px;
-}
-.linkto{
-  margin-top: 20px
+.post {
+	text-align: left;
+	width: 100%;
+	margin-left: 50%;
+	transform: translateX(-50%);
 }
 </style>
-<script>
 
+<script>
 import Listing from "@/components/Listing.vue";
 
 export default {
-  name: "Post",
-  components: {
-    Listing
-  }
+	name: "Post",
+	components: {
+		Listing,
+	},
 };
 </script>
