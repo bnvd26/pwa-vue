ee684321e23d808aae834471686ad18db7357b26
Benjamin <benjaminad5@yahoo.com>
2020-05-20 02:01:35 +0200

    refacto: import new components

--- src/components/Listing.vue
+++ src/components/Listing.vue
@@ -1,23 +1,30 @@
 <template>
-  <div>
-    <div v-for="post in posts" :key="post.id">
-      <router-link :to="`/post/${post.id}`">{{post.title}}</router-link>
-    </div>
-  </div>
+	<div>
+		<div v-for="post in posts" :key="post.id">
+			<CardArticle
+				v-bind:title="post.title"
+				v-bind:id="post.id"
+				v-bind:body="post.body"
+			></CardArticle>
+		</div>
+	</div>
 </template>
 
 <script>
+import CardArticle from "./CardArticle";
 export default {
-  name: "Listing",
-  data() {
-    return { posts: null };
-  },
-  props: {},
-  created() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(response => (this.state = response.json()))
-      .then(json => (this.posts = json));
-  }
+	name: "Listing",
+	components: {
+		CardArticle,
+	},
+	data() {
+		return { posts: null };
+	},
+	props: {},
+	created() {
+		fetch("https://jsonplaceholder.typicode.com/posts")
+			.then((response) => response.json())
+			.then((json) => (this.posts = json));
+	},
 };
 </script>
-
